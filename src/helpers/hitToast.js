c131cdbf92a1f0938364932380083781b41d0c0c
Md. Sazzad Hossain <96966545+Sazz07@users.noreply.github.com>
2023-05-03 21:21:35 +0600

    Complete task

---
+++ src/helpers/hitToast.js
@@ -0,0 +1,13 @@
+import { toast } from 'react-toastify';
+const hitToast = (message) => {
+    toast(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
+export default hitToast;
